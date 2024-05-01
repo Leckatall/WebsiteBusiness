ead98898f6d5693bcfc84042a0073f4209c568d5
Leckatall <32340119+Leckatall@users.noreply.github.com>
2024-05-01 15:08:51 +0100

    changed pitch partial to a class to allow for variable pitches

--- partials/pitch.php
+++ partials/pitch.php
@@ -5,13 +5,31 @@
 //the care-free blogger";
 //$img_path = "images/HappyLaptopWoman.jpg";
 //$img_alt = "Businesswoman";
+class Pitch{
+    public $orientation;
+    public $title;
+    public $subtitle;
+    public $img_path;
+    public $img_alt;
 
-echo "
-<section class='pitch pitch-$orientation'>
-    <img src='$img_path' alt='$img_alt'>
-    <h2 class='titles title'>$title</h2>
-    <div class='titles sub-title'>$subtitle</div>
-</section> "
+    function __construct($orientation, $title, $subtitle, $img_path, $img_alt){
+        $this->orientation = $orientation;
+        $this->title = $title;
+        $this->subtitle = $subtitle;
+        $this->img_path = $img_path;
+        $this->img_alt = $img_alt;
+    }
+
+    function construct(){
+        return "
+        <section class='pitch pitch-$this->orientation'>
+    <img src='$this->img_path' alt='$this->img_alt'>
+    <h2 class='titles title'>$this->title</h2>
+    <div class='titles sub-title'>$this->subtitle</div>
+</section> ";
+    }
+}
+
 ?>
 
 
