a077bca3ed06458f99857c28fdfda30e9742e3d5
Leckatall <32340119+Leckatall@users.noreply.github.com>
2025-02-22 22:29:01 +0000

    Added TODO and other shit

--- Core/Database/init_db.php
+++ Core/Database/init_db.php
@@ -2,9 +2,14 @@
 
 namespace Core\Database;
 
-class init_db
-{
-    public function __construct(){
+public function _init_db__($config, $username='root', $password=''){
+    // TODO: run once to set up all the tables
+    $dsn = 'mysql:'. http_build_query($config, '', ';');
 
-    }
+    $connection = new PDO($dsn, $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+
+    $connection->prepare("CREATE TABLE IF NOT EXISTS");
 }
